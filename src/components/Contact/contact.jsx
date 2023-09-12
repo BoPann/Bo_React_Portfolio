3f12e6f4043b9b86047e0cafb7be297e27e9a6c3
BoPann <114901461+BoPann@users.noreply.github.com>
2023-09-12 15:44:02 -0700

    add email functionality

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser';
 import './contact.css'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fzfxbvk', 'template_hch6hez', form.current, 'AilNeJ2sMpjFofe_2')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      alert("Thanks for reaching out! We will be in contact soon :)");
+      e.target.reset();
+  };
+
   return (
     <section id="contact">
         <h1 className="contactTitle">Contact Me</h1>
         <p className="contactDescription">Let's connect! </p>
-        <form action="" className="contactForm">
-            <input type="text" className="name" placeholder='Your Name'/>
-            <input type="email" className="email" placeholder='Your Email'/>
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <input type="text" className="name" name="from_name" placeholder='Your Name'/>
+            <input type="email" className="email" name="from_email" placeholder='Your Email'/>
             <textarea name="message" className="msg" rows="10" placeholder='Your Message'></textarea>
             <button type="submit" value="Send" className="submitBtn">Submit</button>
         </form>
-
 
         {/* <div className="links">
             <img src="" alt="" className="link" />
